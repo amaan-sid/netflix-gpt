48a3dacc913af04a78763bc24b2058e24b03d0d2
Amaan Siddiquie <mohdamaan9450@gmail.com>
2025-06-02 12:04:58 +0530

    Ignore constants.jsx file

--- src/utils/constants.jsx
+++
@@ -1,5 +0,0 @@
-export const API_KEY = "b67f9fec7e1393f8231e7f9d4b6775af"
-
-export const default_pic_men = "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
-
-export const default_pic_movie =  "https://cdn.pixabay.com/photo/2015/12/22/04/00/photo-1103594_1280.png"
